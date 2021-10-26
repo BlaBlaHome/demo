37716f057739443b14ea087dcf644a3c686bb0d6
BlaBlaHome <officialct24@gmail.com>
2021-10-26 17:29:06 +0530

    shell script program for addition of two numbers

---
+++ add.sh
@@ -0,0 +1,24 @@
+read -p "Enter first number : " a
+read -p "Enter second number : " b
+
+
+
+let x=a+b
+echo $x
+
+
+read -p "Enter first number : " a
+read -p "Enter second number : " b
+
+
+
+let x=a+b
+echo $x
+
+read -p "Enter first number : " a
+read -p "Enter second number : " b
+
+
+
+let x=a+b
+echo $x
